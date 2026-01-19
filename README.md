3d35d026008ae3f086410fdb0e7ed2e10893bc52
Dennis Chinecherem <densenj@outlook.com>
2026-01-19 15:57:07 +0100

    added folder yrkco_container and updated sql script in yrkesco.sql in physical

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # data_modeling_lab_de25
-Designing a database using CDM, LGM and PDM. Implementation using SQL scripts  and running some subqueries.
+- Designing a database using CDM, LGM and PDM. Implementation using SQL scripts  and running some subqueries.
+
+**Business Requirements**
+
+
+
+**Postgres using Docker compose setup**
--- yh_labb/physical/YrkesCo.sql
+++ yh_labb/physical/YrkesCo.sql
@@ -1,10 +1,10 @@
-CREATE TABLE "Program" (
+CREATE TABLE program(
   "program_id" INTEGER PRIMARY KEY,
   "program_name" "VARCHAR(50)" NOT NULL,
   "description" TEXT
 );
 
-CREATE TABLE "Course" (
+CREATE TABLE course (
   "course_id" INTEGER PRIMARY KEY,
   "course_code" "VARCHAR(20)" UNIQUE NOT NULL,
   "course_name" "VARCHAR(100)" NOT NULL,
@@ -14,13 +14,13 @@ CREATE TABLE "Course" (
                                             so if not 'False' then 'True' """
 );
 
-CREATE TABLE "Facility" (
+CREATE TABLE facility (
   "facility_id" INTEGER PRIMARY KEY,
   "city_name" "VARCHAR(50)" NOT NULL,
   "address" TEXT NOT NULL
 );
 
-CREATE TABLE "Educational_leader" (
+CREATE TABLE educational_leader (
   "educational_leader_id" INTEGER PRIMARY KEY,
   "first_name" "VARCHAR(50)",
   "last_name" "VARCHAR(50)",
@@ -28,7 +28,7 @@ CREATE TABLE "Educational_leader" (
   "personal_data_id" INTEGER UNIQUE NOT NULL
 );
 
-CREATE TABLE "Class" (
+CREATE TABLE class (
   "class_id" INTEGER PRIMARY KEY,
   "program_id" INTEGER NOT NULL,
   "educational_leader_id" INTEGER NOT NULL,
@@ -36,19 +36,19 @@ CREATE TABLE "Class" (
   "start_year" DATE NOT NULL
 );
 
-CREATE TABLE "Employment_type" (
+CREATE TABLE employment_type(
   "employment_type_id" INTEGER PRIMARY KEY,
   "type_name" "VARCHAR(50)" UNIQUE NOT NULL
 );
 
-CREATE TABLE "Personal_data" (
+CREATE TABLE personal_data (
   "personal_data_id" INTEGER PRIMARY KEY,
   "personal_number" "VARCHAR(12)" UNIQUE NOT NULL,
   "date_of_birth" TIMESTAMP NOT NULL,
   "address" TEXT NOT NULL
 );
 
-CREATE TABLE "Educator" (
+CREATE TABLE educator (
   "educator_id" INTEGER PRIMARY KEY,
   "first_name" "VARCHAR(50)",
   "last_name" "VARCHAR(50)",
@@ -57,7 +57,7 @@ CREATE TABLE "Educator" (
   "personal_data_id" INTEGER UNIQUE NOT NULL
 );
 
-CREATE TABLE "Student" (
+CREATE TABLE student (
   "student_id" INTEGER PRIMARY KEY,
   "first_name" "VARCHAR(50)",
   "last_name" "VARCHAR(50)",
@@ -66,7 +66,7 @@ CREATE TABLE "Student" (
   "class_id" INTEGER NOT NULL
 );
 
-CREATE TABLE "Company" (
+CREATE TABLE company (
   "company_id" INTEGER PRIMARY KEY,
   "company_name" "VARCHAR(100)",
   "organization_nr" "VARCHAR(20)",
@@ -74,59 +74,59 @@ CREATE TABLE "Company" (
   "address" TEXT
 );
 
-CREATE TABLE "Consultant" (
+CREATE TABLE consultant (
   "consultant_id" INTEGER PRIMARY KEY,
   "educator_id" INTEGER UNIQUE NOT NULL,
   "company_id" INTEGER NOT NULL,
   "hourly_pay" INTEGER NOT NULL
 );
 
-CREATE TABLE "Program_course" (
+CREATE TABLE program_course (
   "program_id" INTEGER,
   "course_id" INTEGER,
   "PRIMARY" "KEY(program_id,course_id)"
 );
 
-CREATE TABLE "Class_course" (
+CREATE TABLE class_course(
   "class_id" INTEGER,
   "course_id" INTEGER,
   "PRIMARY" "KEY(class_id,course_id)"
 );
 
-CREATE TABLE "Educator_course" (
+CREATE TABLE educator_course (
   "educator_id" INTEGER,
   "course_id" INTEGER,
   "PRIMARY" "KEY(educator_id,course_id)"
 );
 
-ALTER TABLE "Educational_leader" ADD FOREIGN KEY ("personal_data_id") REFERENCES "Personal_data" ("personal_data_id");
+ALTER TABLE educational_leader ADD FOREIGN KEY ("personal_data_id") REFERENCES personal_data ("personal_data_id");
 
-ALTER TABLE "Class" ADD FOREIGN KEY ("program_id") REFERENCES "Program" ("program_id");
+ALTER TABLE class ADD FOREIGN KEY ("program_id") REFERENCES program ("program_id");
 
-ALTER TABLE "Class" ADD FOREIGN KEY ("educational_leader_id") REFERENCES "Educational_leader" ("educational_leader_id");
+ALTER TABLE class ADD FOREIGN KEY ("educational_leader_id") REFERENCES educational_leader ("educational_leader_id");
 
-ALTER TABLE "Class" ADD FOREIGN KEY ("facility_id") REFERENCES "Facility" ("facility_id");
+ALTER TABLE class ADD FOREIGN KEY ("facility_id") REFERENCES facility ("facility_id");
 
-ALTER TABLE "Educator" ADD FOREIGN KEY ("employment_type_id") REFERENCES "Employment_type" ("employment_type_id");
+ALTER TABLE educator ADD FOREIGN KEY ("employment_type_id") REFERENCES employment_type ("employment_type_id");
 
-ALTER TABLE "Educator" ADD FOREIGN KEY ("personal_data_id") REFERENCES "Personal_data" ("personal_data_id");
+ALTER TABLE educator ADD FOREIGN KEY ("personal_data_id") REFERENCES personal_data ("personal_data_id");
 
-ALTER TABLE "Student" ADD FOREIGN KEY ("personal_data_id") REFERENCES "Personal_data" ("personal_data_id");
+ALTER TABLE student ADD FOREIGN KEY ("personal_data_id") REFERENCES personal_data ("personal_data_id");
 
-ALTER TABLE "Student" ADD FOREIGN KEY ("class_id") REFERENCES "Class" ("class_id");
+ALTER TABLE student ADD FOREIGN KEY ("class_id") REFERENCES class ("class_id");
 
-ALTER TABLE "Consultant" ADD FOREIGN KEY ("educator_id") REFERENCES "Educator" ("educator_id");
+ALTER TABLE consultant ADD FOREIGN KEY ("educator_id") REFERENCES educator ("educator_id");
 
-ALTER TABLE "Consultant" ADD FOREIGN KEY ("company_id") REFERENCES "Company" ("company_id");
+ALTER TABLE consultant ADD FOREIGN KEY ("company_id") REFERENCES company ("company_id");
 
-ALTER TABLE "Program_course" ADD FOREIGN KEY ("program_id") REFERENCES "Program" ("program_id");
+ALTER TABLE program_course ADD FOREIGN KEY ("program_id") REFERENCES program ("program_id");
 
-ALTER TABLE "Program_course" ADD FOREIGN KEY ("course_id") REFERENCES "Course" ("course_id");
+ALTER TABLE program_course ADD FOREIGN KEY ("course_id") REFERENCES course ("course_id");
 
-ALTER TABLE "Class_course" ADD FOREIGN KEY ("class_id") REFERENCES "Class" ("class_id");
+ALTER TABLE class_course ADD FOREIGN KEY ("class_id") REFERENCES class ("class_id");
 
-ALTER TABLE "Class_course" ADD FOREIGN KEY ("course_id") REFERENCES "Course" ("course_id");
+ALTER TABLE class_course ADD FOREIGN KEY ("course_id") REFERENCES course ("course_id");
 
-ALTER TABLE "Educator_course" ADD FOREIGN KEY ("educator_id") REFERENCES "Educator" ("educator_id");
+ALTER TABLE educator_course ADD FOREIGN KEY ("educator_id") REFERENCES educator ("educator_id");
 
-ALTER TABLE "Educator_course" ADD FOREIGN KEY ("course_id") REFERENCES "Course" ("course_id");
+ALTER TABLE educator_course ADD FOREIGN KEY ("course_id") REFERENCES course ("course_id");
